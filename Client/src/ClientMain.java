15cc9b8efe22206ea71debe7d59ce9ed1021724d
Emilio Vecchi <obaf00@gmail.com>
2025-12-20 20:07:13 +0100

    testing RMI

--- Client/src/ClientMain.java
+++ Client/src/ClientMain.java
@@ -1,7 +1,14 @@
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class ClientMain {
-    public static void main(String[] args){
-        //Registry registry = Service
+    public static void main(String[] args) throws NotBoundException, RemoteException {
+        Registry registry = LocateRegistry.getRegistry();
+        Service server = (Service) registry.lookup("ServerService");
+
+        String response = server.sendMessage("message");
+        System.out.println(response);
     }
 }
--- common/src/ServiceImpl.java
+++ common/src/ServiceImpl.java
@@ -1,6 +1,7 @@
 public class ServiceImpl implements Service {
     @Override
     public String sendMessage(String clientMessage) {
-        return "Client Message".equals(clientMessage) ? "Server Message" : null;
+        System.out.println(clientMessage);
+        return "Server response";
     }
 }
