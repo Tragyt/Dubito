feb19242198448cbfca8dd4defb3179b9cdd254a
git_deploy.py <git_deploy>
2025-12-20 17:14:38 +0100

    common

--- Client/src/ClientMain.java
+++ Client/src/ClientMain.java
@@ -2,6 +2,6 @@ import java.rmi.registry.Registry;
 
 public class ClientMain {
     public static void main(String[] args){
-        Registry registry = ServerSer
+        //Registry registry = Service
     }
 }
